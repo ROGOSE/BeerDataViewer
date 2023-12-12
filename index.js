6e96fcff427e87d703144497b67a697c7c17812d
rodrigo <rodgom02@ucm.es>
2023-12-12 15:44:02 +0100

    1

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var width = $(window).width();
 window.onscroll = function(){
 if ((width >= 1000)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#000");    //antes estaba #fff
-        $("#header").css("color","#fff");           //antes estaba #000
+        $("#header").css("background","#fff");    //antes estaba #fff
+        $("#header").css("color","#000");           //antes estaba #000
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
         $("#header").css("padding","4vh 4vw");
         //$("#header").css("width","100%");
