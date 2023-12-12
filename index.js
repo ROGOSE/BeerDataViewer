a61413033fc658f89c932a10c810a74fc7336fe3
rodrigo <rodgom02@ucm.es>
2023-12-12 15:05:54 +0100

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var width = $(window).width();
 window.onscroll = function(){
 if ((width >= 1000)){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#000");    //antes estaba #fff
+        $("#header").css("background","rgba(128,128,128,0.5)");    //antes estaba #fff
         $("#header").css("color","#fff");           //antes estaba #000
         $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
         $("#header").css("padding","4vh 4vw");
@@ -12,7 +12,7 @@ if ((width >= 1000)){
             $(this).css("border-bottom","2px solid transparent");
         });
     }else{
-        $("#header").css("background","transparent");
+        $("#header").css("background","rgba(128,128,128,0.5)");   //antes estaba "transparent"
         $("#header").css("color","#fff");
         $("#header").css("box-shadow","0px 0px 0px rgba(0, 0, 0,0.09);");    //rgba(0,0,0,0) es negro
         $("#header").css("padding","6vh 4vw");
